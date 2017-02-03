7cc3c8b233bd7191ed21220488ccb52a457acc1f
sensei100 <jenanton01@gmail.com>
2017-02-03 12:48:11 -0500

    initial commit

---
+++ strings.js
@@ -0,0 +1,6 @@
+var firstName = 'Jen';
+var lastName = 'Anton';
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(fullName.length);
---
+++ variables.js
@@ -0,0 +1,3 @@
+var name = "Jen";
+
+console.log(name);
