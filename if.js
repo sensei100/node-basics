ce028bf40e8872f5d6daa3551d995fcd56fdfa2d
sensei100 <jenanton01@gmail.com>
2017-02-05 14:16:16 -0500

    if statements

---
+++ if.js
@@ -0,0 +1,9 @@
+var friend;
+
+if (friend === 'Athena' || friend === 'Elena') {
+  console.log('Hi there!');
+} else if (friend === 'Courtney') {
+  console.log('Hey!');
+} else {
+  console.log('Hi, stranger!');
+}
